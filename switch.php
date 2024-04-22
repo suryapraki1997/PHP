2b77a26ddedcb60fe519fbba186480a288757579
suryapraki1997 <143630486+suryapraki1997@users.noreply.github.com>
2024-04-22 19:40:26 +0530

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,25 @@
+<?php
+$day = date("n");
+switch ($day){
+    case 1:
+    print("sunday");
+    break;
+    case 2:
+    print("monday");
+    break;
+    case 3:
+    print("tuesday");
+    break;
+    case 4:
+    print("wednesday");
+    break;
+    case 5:
+    print("thursday");
+    break;
+    case 6:
+    print("friday");
+    break;
+    case 7:
+    print("saturday");
+    break;
+}
